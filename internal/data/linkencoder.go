cd1874c42b06b949d86467d68172a103ef47bf99
Daniel Michaels <32674032+danielmichaels@users.noreply.github.com>
2021-10-15 08:17:39 +1100

    Add a URLEncoder to generate a unique hash per shortened link

---
+++ internal/data/linkencoder.go
@@ -0,0 +1,30 @@
+package data
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const (
+	alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	length   = uint64(len(alphabet))
+)
+
+// EncodeLink creates a hash based on the number passed in.
+func EncodeLink(number uint64) string {
+	var encodedBuilder strings.Builder
+	encodedBuilder.Grow(11)
+
+	for ; number > 0; number = number / length {
+		encodedBuilder.WriteByte(alphabet[(number % length)])
+	}
+
+	return encodedBuilder.String()
+}
+
+// CreateURL creates a hash which is used for mapping to a URL.
+func CreateURL() string {
+	hash := EncodeLink(uint64(time.Now().UTC().Unix() * rand.Int63()))
+	return hash
+}
