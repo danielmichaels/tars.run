c9f1c637d6a0502e85757d483a60e5681938da70
danielmichaels <dan@danielms.site>
2022-03-17 20:58:05 +1100

    Copy to clipboard alpinejs added

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import Alpine from 'alpinejs';
+import Clipboard from "@ryangjchandler/alpine-clipboard"
 
+Alpine.plugin(Clipboard.configure({
+    onCopy: () => {
+        alert("Copied to Clipboard")
+    }
+}))
 window.Alpine = Alpine;
 Alpine.start();
