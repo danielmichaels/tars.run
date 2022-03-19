b6b22fae495b250d8c087e2ee9aed87ed0947cc2
danielmichaels <dan@danielms.site>
2022-03-19 11:53:15 +1100

    Add collapse plugin to Alpine

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import Alpine from 'alpinejs';
+import collapse from '@alpinejs/collapse'
 import Clipboard from "@ryangjchandler/alpine-clipboard"
 
+Alpine.plugin(collapse)
 Alpine.plugin(Clipboard.configure({
     onCopy: () => {
         alert("Copied to Clipboard")
